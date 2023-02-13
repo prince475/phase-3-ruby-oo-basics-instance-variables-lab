6ed73ffdbec0bb2db05ebfd03d007311cc68b460
prince475 <danielmwai475@gmail.com>
2023-02-13 07:31:47 +0300

    Completed  Lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,11 @@
 class Dog
+
+  # Settter method for setting the name of the dog and displaying it
    def name=(dog_name)
     @this_dogs_name = dog_name
    end
 
+   # getter method for getting the name of the dog
    def name
     @this_dogs_name
    end
